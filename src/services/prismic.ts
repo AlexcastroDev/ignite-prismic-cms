b06d2507ad3fdc53d4f613c743d86aa2f3c1b3f5
Alexandro <alexoliveira7x@gmail.com>
2022-07-09 23:35:30 +0100

    chore: update project

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,18 +1,21 @@
 import * as prismic from '@prismicio/client';
-import { HttpRequestLike } from '@prismicio/client';
-import { enableAutoPreviews } from '@prismicio/next';
 
-export interface PrismicConfig {
-  req?: HttpRequestLike;
-}
+// Fill in your repository name
+export const repositoryName = 'alexandrocastro';
 
-export function getPrismicClient(config: PrismicConfig): prismic.Client {
-  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
+export const client = prismic.createClient(repositoryName, {
+  accessToken: '',
 
-  enableAutoPreviews({
-    client,
-    req: config.req,
-  })
-
-  return client;
-}
+  // This defines how you will structure URL paths in your project.
+  // Update the types to match the Custom Types in your project, and edit
+  // the paths to match the routing in your project.
+  //
+  // If you are not using a router in your project, you can change this
+  // to an empty array or remove the option entirely.
+  routes: [
+    {
+      type: 'homepage',
+      path: '/',
+    },
+  ],
+});
